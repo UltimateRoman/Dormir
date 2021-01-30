9a14f10cd9fdc1732d9264a21b088d3e909b31c0
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-01-30 13:59:52 +0530

    Improved python app

--- Python_App/main.py
+++ Python_App/main.py
@@ -5,8 +5,9 @@ from boltiot import Bolt
 
 db = SQL("sqlite:///maindb.db")
 
-api_key = "Your BOLT API key"
-device_id = "Your BOLT device ID"
+api_key = "9c8c9edd-132b-4d96-a92b-0089580ebd1a"
+device_id = "BOLT7420916"
+uname = input("Enter DORMIR username: ")
 
 bolt_inst = Bolt(api_key, device_id)
 
@@ -18,21 +19,28 @@ def main():
     while True:
         try:
             read_resp = bolt_inst.serialRead('10')
-            read_data = json.loads(read_resp)
-            
-            if read_data['value'] == 'start\n':
-                print(localtime().tm_hour)
-                alarm_on = True
-                
-            if localtime().tm_hour == 8 and alarm_on:
-                if not ring:
-                    bolt_inst.serialWrite('ring')
-                    ring = True
+            read_data = json.loads(read_resp)            
 
-            if ring and read_data['value'] == 'stop\n':
-                print(localtime().tm_hour)
-                ring = False
-                alarm_on = False
+            check_in = db.execute("SELECT in-hour FROM records WHERE date=CURRENT_DATE")
+
+            if not check_in:
+                if read_data['value'] == 'start\n':
+                    db.execute("INSERT INTO records(date, in-hour, in-min) VALUES(current_date, :hour, :mins)", hour=localtime().tm_hour, mins=localtime().tm_min)
+                    alarm_on = True
+
+            else:
+                wakeup_time = db.execute("SELECT wtime FROM users WHERE username=:username",username=uname)[0]['wtime']
+
+                if localtime().tm_hour >= wakeup_time and alarm_on:
+                    if not ring:
+                        bolt_inst.serialWrite('ring')
+                        ring = True
+
+                if ring and read_data['value'] == 'stop\n':
+                    rc = db.execute("SELECT COUNT(*) FROM records")[0]['COUNT(*)']
+                    db.execute("UPDATE records SET out-hour=:ohr AND out-min=:omin WHERE id=:id", ohr=localtime().tm_hour, omin=localtime().tm_min, id=rc)
+                    ring = False
+                    alarm_on = False
             
             sleep(15)
             
